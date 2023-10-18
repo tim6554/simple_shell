9772125de160251d1b64858753f9d2176054946f
tim ermyas <45384341+tim6554@users.noreply.github.com>
2023-10-18 20:28:47 +0300

    Update env1.c

--- env1.c
+++ env1.c
@@ -1,95 +1,19 @@
 #include "shell.h"
-
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in the string, converted number otherwise
  */
-int _myenv(info_t *info)
+
+int _strncmp(char *s1, char *s2, int a)
 {
-	print_list_str(info->env);
-	return (0);
-}
-
-/**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
- */
-char *_getenv(info_t *info, const char *sim)
-{
-	list_t *node = info->env;
-	char *a;
-
-	while (node)
-	{
-		a = starts_with(node->str, sim);
-		if (a && *a)
-			return (a);
-		node = node->next;
-	}
-	return (NULL);
-}
-
-/**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
- */
-int _myunsetenv(info_t *info)
-{
-	int n;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (n = 1; n <= info->argc; n++)
-		_unsetenv(info, info->argv[n]);
-
-	return (0);
-}
-
-/**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
-}
-
-/**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
- */
-int _mysetenv(info_t *info)
-{
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (!a)
 		return (0);
-	return (1);
+	if (*s1 == *s2)
+		return (*s1 ? _strncmp(s1 + 1, s2 + 1, a - 1) : 0);
+	if (*s1)
+		return (1);
+	if (*s2)
+		return (-1);
+	return (*s1 - *s2);
 }
