5eee25bd8b6a12bd715351d94ce8aef33b5ba490
tim ermyas <45384341+tim6554@users.noreply.github.com>
2023-10-18 20:31:46 +0300

    Update er.c

--- er.c
+++ er.c
@@ -1,89 +1,21 @@
 #include "shell.h"
-
 /**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *_isalpha - checks for alphabetic character
+ *@s: input
+ *Return: 1 if aplha, 0 otherwise
  */
-void _eputs(char *rst)
+char *_strcpy(char *fin, char *men)
 {
-	int n = 0;
+	int n, cnt = 0;
 
-	if (!rst)
-		return;
-	while (rst[n] != '\0')
+	while (men[cnt] != '\0')
 	{
-		_eputchar(rst[n]);
-		n++;
+		cnt++;
 	}
-}
-
-/**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _eputchar(char a)
-{
-	static int n;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (a == BUF_FLUSH || n >= WRITE_BUF_SIZE)
+	for (i = 0; n < cnt; n++)
 	{
-		write(2, buf, n);
-		n = 0;
+		fin[n] = men[n];
 	}
-	if (a != BUF_FLUSH)
-		buf[n++] = a;
-	return (1);
-}
-
-/**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putfd(char b, int d)
-{
-	static int n;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (b == BUF_FLUSH || n >= WRITE_BUF_SIZE)
-	{
-		write(d, buf, n);
-		n = 0;
-	}
-	if (b != BUF_FLUSH)
-		buf[n++] = b;
-	return (1);
-}
-
-/**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putsfd(char *rst, int d)
-{
-	int n = 0;
-
-	if (!rst)
-		return (0);
-	while (*rst)
-	{
-		n += _putd(*rst++, d);
-	}
-	return (n);
+	fin[n] = '\0';
+	return (fin);
 }
