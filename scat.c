404d559ae8dd07c87712678dbd1783b81a860746
tim ermyas <45384341+tim6554@users.noreply.github.com>
2023-10-20 23:10:37 +0300

    Update scat.c

--- scat.c
+++ scat.c
@@ -1,26 +1,85 @@
 #include "shell.h"
-/**
- * interactive - is interactive mode
- * @info: address
- *
- * Return: 1 if success, 0 otherwise
- */
-char *_strcat(char *fin, char *men)
-{
-	int n = 0, d = 0, j;
 
-	while (men[n] != '\0')
+/**
+ *_eputs - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
 	{
-		n++;
+		_eputchar(str[i]);
+		i++;
 	}
-	while (fin[d] != '\0')
+}
+
+/**
+ * _eputchar - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		d++;
+		write(2, buf, i);
+		i = 0;
 	}
-	for (j = 0; j < n; j++)
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes the character c to given fd
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		fin[d + j] = men[j];
+		write(fd, buf, i);
+		i = 0;
 	}
-	fin[d + n] = '\0';
-	return (fin);
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
