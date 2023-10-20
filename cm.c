5b287c1a4ab8398cae708a8d4fcdacea2af25350
tim ermyas <45384341+tim6554@users.noreply.github.com>
2023-10-20 23:08:00 +0300

    Update cm.c

--- cm.c
+++ cm.c
@@ -1,38 +1,74 @@
 #include "shell.h"
+
 /**
- * is_delim - checks if delimeter
- * @b: to check
- * @dlm: the delimeter str
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-void runcmd(char **arv, char **rgv, char **envp)
+int is_delim(char c, char *delim)
 {
-	pid_t child;
-	int s;
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
 
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
 
-	child = fork();
-
-	if (child == -1)
-	{
-		perror("Process Error");
-		exit(EXIT_FAILURE);
-	}
-	if (child == 0)
-	{
-
-		if (execve(rgv[0], rgv, envp) == (-1))
-		{
-			write(STDOUT_FILENO, arv[0], _strlen(arv[0]));
-			write(STDOUT_FILENO, ": No such file or directory",
-			_strlen(": No such file or directory"));
-			write(STDOUT_FILENO, "\n", 1);
-			
-		}
-		exit(EXIT_FAILURE);
-	}
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
 	else
+		return (0);
+}
+
+/**
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
-		wait(&s);
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
 	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
